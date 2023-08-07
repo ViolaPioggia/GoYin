a079ad1aa60a1f5b0b05ed6a7d036e6931d3d6b0
KeiichiKasai <1913683154@qq.com>
2023-08-07 12:05:21 +0800

    (feat)add some mysql interfaces

--- server/service/user/dao/mysql.go
+++ server/service/user/dao/mysql.go
@@ -1,8 +1,10 @@
 package dao
 
 import (
+	"GoYin/server/common/consts"
 	"GoYin/server/service/user/models"
 	"context"
+	"errors"
 	"github.com/cloudwego/kitex/pkg/klog"
 	"gorm.io/gorm"
 )
@@ -12,11 +14,68 @@ type User struct {
 }
 
 func (u User) CreateUser(ctx context.Context, user *models.User) error {
-	//TODO implement me
-	return nil
+	tx := u.db.Begin() //开启事务
+
+	if tx.Error != nil { //检查事务是否正常开启
+		tx.Rollback()
+		return tx.Error
+	}
+
+	select {
+	case <-ctx.Done(): //若收到取消信号则退出
+		tx.Rollback()
+		return ctx.Err()
+	default:
+		var temp models.User
+		err := u.db.Where("username = ?", user.Username).First(&temp).Error
+		if err != nil {
+			klog.Errorf("mysql select failed,", err)
+			tx.Rollback()
+			return err
+		}
+		if temp.Username != "" {
+			err = errors.New(consts.MysqlAlreadyExists)
+			tx.Rollback()
+			return err
+		}
+		err = u.db.Create(&user).Error
+		if err != nil {
+			klog.Errorf("mysql insert failed", err)
+			tx.Rollback()
+			return err
+		}
+
+		if err = tx.Commit().Error; err != nil { //提交事务并判断是否成功提交
+			tx.Rollback()
+			return err
+		}
+		return nil
+	}
 }
 func (u User) GetUserByUsername(ctx context.Context, username string) (*models.User, error) {
-	return nil, nil
+	tx := u.db.Begin() //开启事务
+
+	if tx.Error != nil { //检查事务是否正常开启
+		tx.Rollback()
+		return nil, tx.Error
+	}
+
+	select {
+	case <-ctx.Done():
+		tx.Rollback()
+		return nil, ctx.Err()
+	default:
+		var user models.User
+		if err := u.db.Where("username = ?", username).First(&user).Error; err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+		if err := tx.Commit().Error; err != nil { //提交事务并判断是否成功提交
+			tx.Rollback()
+			return nil, err
+		}
+		return &user, nil
+	}
 }
 func NewUser(db *gorm.DB) *User {
 	m := db.Migrator()
--- server/service/user/models/user.go
+++ server/service/user/models/user.go
@@ -2,7 +2,7 @@ package models
 
 type User struct {
 	ID              int64
-	Username        string
+	Username        string `gorm:"index:idx_username,unique"`
 	Password        string
 	Avatar          string
 	BackGroundImage string
