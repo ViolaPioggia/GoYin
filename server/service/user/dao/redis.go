08bf946986d094ee70c5df5ccaec8f9685b19f44
violapioggia <2604296771@qq.com>
2023-08-16 14:37:30 +0800

    (feat) add user redis interface

--- server/service/user/dao/redis.go
+++ server/service/user/dao/redis.go
@@ -3,7 +3,10 @@ package dao
 import (
 	"GoYin/server/service/user/model"
 	"context"
+	"github.com/bytedance/sonic"
+	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/go-redis/redis/v8"
+	"strconv"
 )
 
 type RedisManager struct {
@@ -15,13 +18,49 @@ func NewRedisManager(client *redis.Client) *RedisManager {
 }
 
 func (r *RedisManager) CreateUser(ctx context.Context, user *model.User) error {
+	userJson, err := sonic.Marshal(user)
+	if err != nil {
+		klog.Error("redis marshal user failed,", err)
+		return err
+	}
+	err = r.redisClient.Set(ctx, "user:"+strconv.FormatInt(user.ID, 10), userJson, 0).Err()
+	if err != nil {
+		klog.Error("redis create user failed,", err)
+		return err
+	}
 	return nil
 }
 
 func (r *RedisManager) GetUserById(ctx context.Context, id int64) (*model.User, error) {
-	return nil, nil
+	userJson, err := r.redisClient.Get(ctx, "user:"+strconv.FormatInt(id, 10)).Bytes()
+	if err != nil {
+		klog.Error("redis get user by id failed,", err)
+		return nil, err
+	}
+	var user *model.User
+	err = sonic.Unmarshal(userJson, &user)
+	if err != nil {
+		klog.Error("redis unmarshal user failed,", err)
+		return nil, err
+	}
+	return user, nil
 }
 
 func (r *RedisManager) BatchGetUserById(ctx context.Context, id []int64) ([]*model.User, error) {
-	return nil, nil
+	var userList []*model.User
+	for _, v := range id {
+		userJson, err := r.redisClient.Get(ctx, "user:"+strconv.FormatInt(v, 10)).Bytes()
+		if err != nil {
+			klog.Error("redis get user by id failed,", err)
+			return nil, err
+		}
+		var user *model.User
+		err = sonic.Unmarshal(userJson, &user)
+		if err != nil {
+			klog.Error("redis unmarshal user failed,", err)
+			return nil, err
+		}
+		userList = append(userList, user)
+	}
+	return userList, nil
 }
